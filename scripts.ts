4c95c712c0ca32467c282ee3174d121d34b51ce2
BanuDanabakiyam <banubala9655@gmail.com>
2023-06-30 19:38:44 +0530

    FindUniquedata

--- scripts.ts
+++ scripts.ts
@@ -7,19 +7,28 @@ async function main() {
 //   const user = await prisma.user.create({ data: { name: "Chandrasekar" }})
 //     const users = await prisma.user.deleteMany()
 //     console.log(users); 
-      await prisma.user.deleteMany();
-      const user = await prisma.user.createMany({ 
-         data: [{
-            name: "Dinesh",
-            email: "1234@gmail.com",
-            age: 27,
-            },{
-             name: "Banu",
-             email: "12345@gmail.com",
-             age: 28 ,  
-            }]
-         })
-      console.log(user);
+      await prisma.user.deleteMany(); // avoid to repeated data
+      // ********* Data Reading **********//
+    //   const user = await prisma.user.createMany({ 
+    //      data: [{
+    //         name: "Dinesh",
+    //         email: "1234@gmail.com",
+    //         age: 27,
+    //         },{
+    //          name: "Banu",
+    //          email: "12345@gmail.com",
+    //          age: 28 ,  
+    //         }]
+    //      })
+    //   console.log(user);
+
+    // **************** UNIQUE KEYS ***********************
+    const user = await prisma.user.findUnique({
+        where: {
+            email: "12345@gmail.com",
+        },
+    })
+    console.log(user);
 }
 
 main()
